eb7ada771c40bb70e371b76091f571c967438450
AlexGian <alexgian@blueyonder.co.uk>
2024-06-20 14:51:01 +0100

    demos in build/src subdirectory

--- src/test2.clj
+++ src/test2.clj
@@ -3,8 +3,12 @@
   :exclude [* + - / = abs compare denominator divide
             infinite? numerator partial ref zero?])
 (:require
+  [emmy.leva :as leva]
+)
+)
+(comment
+(:require
   [emmy.calculus.manifold :as cm]
-  [emmy.clerk :as ec]
   [emmy.env :refer :all]
   [emmy.leva :as leva]
   [emmy.mathbox.plot :as plot]
